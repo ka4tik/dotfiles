0694d4347d740221583d4afb74bd44c9bdb9ce61
Kartik Singal <ka4tik@gmail.com>
2014-09-01 05:27:13 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ sudo apt-get install vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/template.cpp ~/template.cpp
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
