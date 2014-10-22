a5c85a6da2e52e928b2121cbc360924619f04998
Kartik Singal <ka4tik@gmail.com>
2014-10-22 16:24:34 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 sudo apt-get install git
 git clone https://github.com/ka4tik/dotfiles.git
-sudo apt-get install vim
+sudo apt-get install vim-gnome
+mv ~/.vimrc ~/.vimrc.old
+mv -R ~/.vim ~/.vim.old
+mv  ~/.bash_aliases ~/.bash_aliases.old
+mv ~/template.cpp ~/template.cpp.old
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/template.cpp ~/template.cpp
